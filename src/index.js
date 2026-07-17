5e933f11bfcbbc7f7713ebb38232a4e471f08ef5
Acer - Kemal <kemal.satya.wibowo1@gmail.com>
2026-07-17 15:20:39 +0700

    feat(project): init project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    status: "Success",
+    message: "Welcome, Service is Running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
