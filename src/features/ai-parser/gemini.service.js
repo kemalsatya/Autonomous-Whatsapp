427cd0171dd06e1bac89a390c1431e5a73d347e1
Acer - Kemal <kemal.satya.wibowo1@gmail.com>
2026-07-18 14:17:17 +0700

    refactor(gemini): remove unused import of instructionOption

--- src/features/ai-parser/gemini.service.js
+++ src/features/ai-parser/gemini.service.js
@@ -1,6 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
 import { customSystemInstruction } from "./gemini.prompt";
-import { instructionOption } from "./gemini.prompt";
 import "dotenv/config";
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
