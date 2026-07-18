e646a505eb6d9f30a77f35dc661406f0ae7edac9
Acer - Kemal <kemal.satya.wibowo1@gmail.com>
2026-07-18 10:57:15 +0700

    feat(whatsapp): add help reply text and design genai response

--- src/features/whatsapp/whatsapp.controller.js
+++ src/features/whatsapp/whatsapp.controller.js
@@ -1,16 +1,47 @@
 import { client } from "./whatsapp.client";
+import { helpReplyText } from "./text.helper";
 
 client.on("message", async (message) => {
   const messageBody = message.body.trim().toLowerCase();
-  switch (messageBody.startsWith) {
+  const messageInstruction = messageBody.split(" ")[0];
+
+  switch (messageInstruction) {
+    case "help":
+      message.reply(helpReplyText);
+      break;
     case "inisiasi":
+      /* kirim ke genai untuk parse data jadi json seperti ini:
+      {
+        "client":"nama client"
+        "project":"project"
+      }
+      */
+      break;
+    case "daftar":
+      /* kirim ke genai untuk parse data jadi json seperti ini:
+      {
+        ["nama person", "kelompok", "jenis kelamin"]
+      }
+      */
       break;
     case "tambah":
+      /* kirim ke genai untuk parse data jadi json seperti ini:
+      {
+        ["nama person", "hadir/ijin/sakit/absen"]
+      }
+      */
       break;
-    case "hapus":
+    case "pindah":
+      /* kirim ke genai untuk parse data jadi json seperti ini:
+      {
+        ["nama person"]
+      }
+      */
       break;
-    case "laporan":
-      break;
+    // case "hapus":
+    //  break;
+    // case "laporan":
+    //  break;
     default:
       break;
   }
